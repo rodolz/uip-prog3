b3341ad66a532415d9ed1a9fc1cebc02ef30697a
rodolz <skannt@gmail.com>
2015-06-08 21:40:24 -0500

    Se modifico el Quiz#3 -- El profesor hizo una aclaratoria en el problema de comisiones

--- Laboratorios/Sem3/quiz3.py
+++ Laboratorios/Sem3/quiz3.py
@@ -1,5 +1,8 @@
-resp = 17
+vent = 0
 nom = input("Nombre: ")
-ven = input("Ventas: ")
-com = int(ven)*0.125
-print("Vendedor\tVentas\tComision\n--------\t------\t--------\n"+nom+"\t\t"+ven+"\t"+str(com))
+for x in range(0, 3):
+	ven = input("Ventas: ")
+	vent = int(ven)+vent
+
+com = int(vent)*0.125
+print("Vendedor\tVentas\tComision\n--------\t------\t--------\n"+nom+"\t\t"+str(vent)+"\t"+str(com))
